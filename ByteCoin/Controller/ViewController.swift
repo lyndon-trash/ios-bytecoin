7ebda12d96a2cf03512d7dcc1fc82f498763c24c
LM Bibera <lyndon.bibera@headhuntr.io>
2020-08-28 23:02:15 +0800

    Add api request to coinapi.io

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -31,6 +31,13 @@ class ViewController: UIViewController {
 
 extension ViewController: CoinManagerDelegate {
     
+    func coinManager(_ coinManager: CoinManager, didCalculateCurrency currency: Currency, rate: Money) {
+        print("Exchange Rate \(currency): \(rate)")
+    }
+    
+    func coinManager(_ coinManager: CoinManager, didError error: Error) {
+        print("Error: \(error)")
+    }
 }
 
 
@@ -55,7 +62,9 @@ extension ViewController: UIPickerViewDelegate {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print("Picked: \(coinManager.currencyArray[row])")
+        let currency: Currency = coinManager.currencyArray[row]
+        print("Picked: \(currency)")
+        coinManager.fetchBitcoinExchange(forCurreny: currency)
     }
         
 }
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,17 +8,63 @@
 
 import Foundation
 
+typealias Money = Double
+typealias Currency = String
+
 class CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "14D3C306-F29F-4E21-B697-8D3E59375842"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
-
     weak var delegate: CoinManagerDelegate?
+    
+    func fetchBitcoinExchange(forCurreny currency: Currency) {
+        if let url = URL(string: "\(baseURL)/\(currency)") {
+            let session = URLSession(configuration: .default)
+            var request = URLRequest(url: url)
+            request.setValue(apiKey, forHTTPHeaderField: "X-CoinAPI-Key")
+            
+            let task = session.dataTask(with: request) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.coinManager(self, didError: error!)
+                }
+                
+                let parseResult = self.parseJSON(exchangeData: data!)
+                
+                if let safeResult = parseResult.0 {
+                    self.delegate?.coinManager(self, didCalculateCurrency: currency, rate: safeResult.rate)
+                    return
+                }
+                
+                if let error = parseResult.1 {
+                    self.delegate?.coinManager(self, didError: error)
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(exchangeData: Data) -> (ExchangeRate?, Error?) {
+        let decoder = JSONDecoder()
+        do {
+            let decoded = try decoder.decode(ExchangeRate.self, from: exchangeData)
+            return (decoded, nil)
+        } catch {
+            return (nil, error)
+        }
+    }
 }
 
 protocol CoinManagerDelegate: AnyObject {
     
+    func coinManager(_ coinManager: CoinManager, didCalculateCurrency: Currency, rate: Money)
+    
+    func coinManager(_ coinManager: CoinManager, didError: Error)
+}
+
+struct ExchangeRate: Codable {
+    let rate: Money
 }
